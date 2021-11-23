841ebb91410570e5db15b532c67662afba9ada65
ProDeveloper <ramazani.david@gmail.com>
2021-11-23 13:20:20 +0330

    first commit

---
+++ Common/DataRetriver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Rmz.WeatherForecast.Api.Common
+{
+    public class ApiCallerHelper
+    {
+
+
+    }
+}
---
+++ Models/ConfigModels/ApiSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rmz.WeatherForecast.Api.Models.ConfigModels
+{
+    public class ApiSettings
+    {
+        public string ApiKey { get; set; }
+        public string ApiUrl { get; set; }
+    }
+}
